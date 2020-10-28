285474af01e9283072d0e9aed5724226644825da
drajat824 <drajat.fikri24@gmail.com>
2020-10-29 01:42:35 +0700

    delete file tidak penting

--- src/routes/adminTransaction.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const { getAllHistory_Admin } = require('../controllers/transfer')
-
-router
-.get('/history', getAllHistory_Admin)
-module.exports = router
