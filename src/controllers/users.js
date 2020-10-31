ca651504ed768235496507f7ce9b7c6fb8ed9ab3
apirahman <apirahman55@gmail.com>
2020-10-31 19:58:13 +0700

    FIX Transfer

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -149,6 +149,7 @@ class Users {
       const transfer = await insertTransfer({ id_receiver: id, note, amount: total, balance: currentBalanceFrom })
       const transactions = await insertTransactions({ id_user: decoded.id, id_transfer: transfer.insertId, type: "transfer", created_at: new Date() });
 
+      console.log(new Date().toDateString())
       return resSuccess(res, OK, "Success Transfer", { id: transactions.insertId });
     } catch (error) {
       console.log(error)
