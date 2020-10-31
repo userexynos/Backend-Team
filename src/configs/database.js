691b52942b0c7c234bc39aeaa1190840c762d0f6
apirahman <apirahman55@gmail.com>
2020-11-01 06:02:22 +0700

    Revisi 1

--- src/configs/database.js
+++ src/configs/database.js
@@ -5,5 +5,5 @@ module.exports = mysql.createConnection({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    timezone: "Asia/Jakarta"
+    timezone: "UTC"
 })
